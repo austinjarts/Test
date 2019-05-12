4241c4997cfde3b1ace7e9af4bb875303ac5297b
Austin Arts <austinjarts@gmail.com>
2019-05-12 20:21:13 +0000

    Initial commit

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,12 +1,26 @@
 class QuotesController < ApplicationController
 
 
-  def index
-   @quote = Quote.all.sample
+def index
+ @quote = Quote.all.sample
+end
+
+def new
+  @quote = Quote.new
   end
 
-  def new
-    @quote = Quote.new
+def create
+  @quote = Quote.create(quote_params)
+  if @quote.invalid?
+    flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
   end
+  redirect_to root_path
+end
+
+private
+
+def quote_params
+  params.require(:quote).permit(:saying, :author)
+end
 
 end
